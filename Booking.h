70db775c7f4b4176ab97d439b874bdb6754ad38f
Vova <vladyegorin@gmail.com>
2025-07-06 16:17:42 -0700

    booking.h format

--- Booking.h
+++ Booking.h
@@ -18,23 +18,42 @@ This module is used to store data about bookings.
 #include <string>
 using namespace std;
 
+//----------------------------------------------------------------------------
+
 class Booking {
-private:
-    string SailingId;
-    string licensePlate;
-    bool checkedIn = false;//at default not checkedIn
-
 public:
-    Booking(const string& licensePlate, const string& SailingId);//createBooking() basically
-    void init();
-    void checkIn(const Booking& booking);
-    bool isCheckedIn(const Booking& booking);
-    void shutDown();
+    Booking(const string& licensePlate,//input
+            const string& SailingId//input
+            );
+    //Job: Booking object constructor.
+    //Usage: Used when a vehicle is getting booked for a Sailing.
+    //Restrictions: License plate out of domain; SailingID must be in the domain and must exist.
+
+    //----------------------------------------------------------------------------
+
+    void checkIn(const Booking& booking//input
+                );
+    //Job: Checks in a vehicle for a Sailing.
+    //Usage: Used when a booked vehicle needs to be checked in upon arrival to the port.
+    //Restrictions: Vehicle must be first booked to be checked in.
+
+    //----------------------------------------------------------------------------
+
+    bool isCheckedIn(const Booking& booking//input
+                    );
+    //Job: Returns a boolean flag indicating if the vehicle is booked in.
+    //Usage: Call when need to find out if the vehicle is booked.
+    //Restrictions: Booking must exist.
+
+    //----------------------------------------------------------------------------
 
 
-    //deleteBooking
-    //findBooking
-    //these two shouldn't be here
+private:
+    string SailingId;//SailingID is used to identify on which Sailing the vehicle is booked
+    string licensePlate;//vehicle's license plate. acts as a primary key
+    bool checkedIn = false;//Flag to identify if vehicle has been registered
+
+
 
 
 };
