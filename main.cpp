8e68630c6b7791199ef06e6dbc7906f728edeff1
Vova <vladyegorin@gmail.com>
2025-07-06 15:07:23 -0700

    coding conventions in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,3 +6,28 @@ int main(){
     cout << "Hello World!" << endl;
     cout<<"Hello World!"<<endl;
 }
+
+//==========================================================================
+//==========================================================================
+/*
+MODULE NAME:
+REVISION HISTORY. Revisions are going to be descending e.g.
+Rev.2 -
+Rev.1 -
+----------------------------------------------------------------------------
+Explanation of what the module does and where it is used.
+----------------------------------------------------------------------------
+C++ #include statements go here.
+----------------------------------------------------------------------------
+Global constants/type/variables go here.
+----------------------------------------------------------------------------
+Class declarations go here.
+Public members first, protected and private go below that.
+----------------------------------------------------------------------------
+EXAMPLE FUNCTION DECLARATION:
+Booking(const string& licensePlate, //first parameter comment example
+        const string& SailingId     //second parameter comment example
+        );
+
+
+*/
