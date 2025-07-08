5920b352ee56c2fcc24751795c8f2825e0f785e2
Vova <vladyegorin@gmail.com>
2025-07-07 20:39:11 -0700

    sailing and vehicle headers created

---
+++ Sailing.h
@@ -0,0 +1,8 @@
+//
+// Created by Vova on 7/7/2025.
+//
+
+#ifndef SAILING_H
+#define SAILING_H
+
+#endif //SAILING_H
---
+++ Vehicle.h
@@ -0,0 +1,8 @@
+//
+// Created by Vova on 7/7/2025.
+//
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#endif //VEHICLE_H
