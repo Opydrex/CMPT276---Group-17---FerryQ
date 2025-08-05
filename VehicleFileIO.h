3ca174250a13cb9e3d34f3ebfd29156e7779432a
Opydrex <soliman-aly@outlook.com>
2025-08-05 02:13:53 -0700

    Finalized Header

--- VehicleFileIO.h
+++ VehicleFileIO.h
@@ -2,7 +2,7 @@
 //
 // MODULE NAME: VehicleFileIO.h
 // Rev.3 - 05/08/2025 - Updated constant values to correctly display in prints
-// Rev.2 - 24/07/2025 - Minor changes to function declarations
+// Rev.2 - 24/07/2025 - Minor changes to comments
 // Rev.1 - 09/07/2025 - Vehicle class header created
 // 
 // ----------------------------------------------------------------------------
